e81434dcc65b496a881ee5ec884922080a69392a
Galina Marakina <g.marakina.2gis@gmail.com>
2020-05-06 18:28:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/taskdates/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.taskdates;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.icu.util.GregorianCalendar;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mChooseStartDate;
+    private Button mChooseEndDate;
+    private CalendarView mStartDateCalendar;
+    private CalendarView mEndtDateCalendar;
+    private Button mBtnOK;
+
+    private long mStartDate;
+    private String mStartDateTxt;
+    private long mEndDate;
+    private String mEndDateTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mChooseStartDate = findViewById(R.id.chooseStartDate);
+        mChooseEndDate = findViewById(R.id.chooseEndDate);
+        mStartDateCalendar = findViewById(R.id.startDateCalendar);
+        mEndtDateCalendar = findViewById(R.id.endtDateCalendar);
+        mBtnOK = findViewById(R.id.btnOK);
+
+        // Скроем календари при запуске приложения
+        mStartDateCalendar.setVisibility(View.GONE);
+        mEndtDateCalendar.setVisibility(View.GONE);
+
+        mChooseStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mStartDateCalendar.setVisibility(View.VISIBLE);
+                mEndtDateCalendar.setVisibility(View.GONE);
+            }
+        });
+
+        mChooseEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEndtDateCalendar.setVisibility(View.VISIBLE);
+                mStartDateCalendar.setVisibility(View.GONE);
+            }
+        });
+
+        mStartDateCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                mStartDateTxt = i+"-"+i1+"-"+i2;
+                mChooseStartDate.setText(R.string.start_time + mStartDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                mStartDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+            }
+        });
+
+        mEndtDateCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                mEndDateTxt = i+"-"+i1+"-"+i2;
+                mChooseEndDate.setText(R.string.end_time + mEndDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                mEndDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+            }
+        });
+
+        mBtnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStartDate > mEndDate){
+                    Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_LONG).show();
+                    mChooseStartDate.setText(R.string.start_time);
+                    mChooseEndDate.setText(R.string.end_time);
+                } else {
+                    Toast.makeText(MainActivity.this, getString(R.string.start)
+                            + mStartDateTxt + " " + getString(R.string.end)
+                            + mEndDateTxt, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
